b8bf170f88adf64d31b9daf501d895d1620902b2
Trang Le <trang.thule@zoho.com>
2024-11-11 11:01:45 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
+import { useEffect } from 'react';
+import { Flex, Layout } from 'antd'
+import SearchGroup from './SearchGroup'
 import './App.css';
 
+const { Content } = Layout
+
+const layoutStyle = {
+  backgroundColor: 'inherit',
+  height: '100vh'
+}
+
+const contentStyle = {
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  maxWidth: '720px'
+}
 function App() {
+  useEffect(() => {
+    document.title = 'Cook This App'
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex gap="middle" wrap>
+      <Layout style={layoutStyle}>
+        <Content style={contentStyle}>
+          <h1>Cook This App</h1>
+          <p style={{
+            fontSize: '1.25rem',
+            margin: '0 0 1rem 0'
+          }}>
+            A little AI tool that allows you to search for recipes based on <em>your intention</em>.
+          </p>
+          <SearchGroup />
+        </Content>
+      </Layout>
+    </Flex>
   );
 }
 
---
+++ src/SearchGroup.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from 'react'
+import { Card, Flex, Input, Tabs } from 'antd'
+
+const { Search } = Input
+
+// const onTabClick = (key, label) => {
+//   console.log(`Tab chosen: `, key)
+// }
+// function SearchBar() {
+//   return (
+//   <Search
+//     placeholder='Example query: asian savory food'
+//     onSearch={onSearch}
+//     style={{
+//       width: 200
+//     }}
+//   />
+//   )
+// }
+const items = [
+  {
+    key: '1',
+    label: 'Search',
+    children: 'Search for a recipe without having to use the precise words'
+  },
+  {
+    key: '2',
+    label: 'Ask',
+    children: 'Ask questions that goes beyond recipe search'
+  }
+]
+
+const placeholderDict = {
+  'search': 'e.g. savory asian dish',
+  'ask': 'e.g. what dish uses the most meat'
+}
+
+function SearchGroup() {
+  const [currentTabKey, setTabKey] = useState('1')
+  const [placeholderText, setPlaceholderText] = useState(placeholderDict['search'])
+  const [loading, setLoading] = useState(false)
+  const [results, setResults] = useState([])
+  const [query, setQuery] = useState('')
+  useEffect(() => {
+    if (!loading || query === '') return
+    const selectedFeature = items.find(item => item.key === currentTabKey).label.toLowerCase()
+    console.log({ selectedFeature })
+    const jsonBody = JSON.stringify({
+      query
+    })
+    console.log(jsonBody)
+    const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:4000" : ""
+    fetch(`${baseUrl}/${selectedFeature}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonBody
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      setResults(data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+    .finally(() => {
+      setQuery('')
+      setLoading(false)
+    })
+  }, [loading, query, results])
+  function onTabClick(clickedKey) {
+    console.log(`Tab key selected: `, clickedKey)
+    if (currentTabKey !== clickedKey) {
+      setTabKey(clickedKey)
+      const selectedFeature = items.find(item => item.key === clickedKey).label.toLowerCase()
+      setPlaceholderText(placeholderDict[selectedFeature])
+    }
+  }
+  function onSearch(value, _e, info) {
+    console.log({ value })
+    console.log({ currentTabKey })
+    // const selectedFeature = items.find(item => item.key === currentTabKey).label.toLowerCase()
+    // console.log({ selectedFeature })
+    // const jsonBody = JSON.stringify({
+    //   feature: selectedFeature,
+    //   query: value
+    // })
+    // console.log(jsonBody)
+    setLoading(true)
+    setQuery(value)
+}
+  function renderResults() {
+    if (currentTabKey === "1") {
+      return (
+        <Flex wrap gap="large">
+          {results.map(result => {
+            const chunk = result.chunk.split(/:(.*)/s)
+            return <Card title={chunk[0]} style={{ width: 300 }}><p>{chunk[1]}</p></Card> 
+          })}
+        </Flex>
+      )
+    }
+    return results.map((result, index) => <p key={index}>{result.generate_rag_response}</p>)
+  }
+    return (
+      <>
+      <Tabs 
+        style={{ marginBottom: '1rem' }} 
+        defaultActiveKey='1' items={items}  
+        onChange={onTabClick}
+      />
+      <Search
+        allowClear
+        loading={loading}
+        onSearch={onSearch}
+        placeholder={placeholderText}
+        style={{
+          marginBottom: '2rem',
+          maxWidth: '480px'
+        }}
+      />
+      {results.length > 0 ? (
+        <div>{renderResults()}</div>
+      ) : <div></div>}
+      </>
+    )
+}
+export default SearchGroup
